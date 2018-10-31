46ebafc96d3f0a637f05b113f0b169503d25f8ee
Gergely Papp <gpapp@hortonworks.com>
2018-10-31 07:31:40 +0100

    BUG-113184 AttachedDisk: checkResources implementation, untested

--- cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/resource/AwsAttachedDiskResourceBuilder.java
+++ cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/resource/AwsAttachedDiskResourceBuilder.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeVolumesRequest;
+import com.amazonaws.services.ec2.model.DescribeVolumesResult;
+import com.sequenceiq.cloudbreak.cloud.aws.AwsClient;
 import com.sequenceiq.cloudbreak.cloud.aws.context.AwsContext;
 import com.sequenceiq.cloudbreak.cloud.aws.service.AwsResourceNameService;
 import com.sequenceiq.cloudbreak.cloud.context.AuthenticatedContext;
@@ -16,10 +23,12 @@ import com.sequenceiq.cloudbreak.cloud.context.CloudContext;
 import com.sequenceiq.cloudbreak.cloud.model.CloudInstance;
 import com.sequenceiq.cloudbreak.cloud.model.CloudResource;
 import com.sequenceiq.cloudbreak.cloud.model.CloudResource.Builder;
+import com.sequenceiq.cloudbreak.cloud.model.CloudResourceStatus;
 import com.sequenceiq.cloudbreak.cloud.model.CloudStack;
 import com.sequenceiq.cloudbreak.cloud.model.Group;
 import com.sequenceiq.cloudbreak.cloud.model.Image;
 import com.sequenceiq.cloudbreak.cloud.model.InstanceTemplate;
+import com.sequenceiq.cloudbreak.cloud.model.ResourceStatus;
 import com.sequenceiq.cloudbreak.cloud.model.Volume;
 import com.sequenceiq.cloudbreak.cloud.model.VolumeSetAttributes;
 import com.sequenceiq.cloudbreak.common.type.CommonStatus;
@@ -28,7 +37,12 @@ import com.sequenceiq.cloudbreak.common.type.ResourceType;
 @Component
 public class AwsAttachedDiskResourceBuilder extends AbstractAwsComputeBuilder {
 
+    private static final String VOLUME_ID = "id";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AwsAttachedDiskResourceBuilder.class);
+
+    @Inject
+    private AwsClient awsClient;
 
     @Override
     public List<CloudResource> create(AwsContext context, long privateId, AuthenticatedContext auth, Group group, Image image) {
@@ -78,6 +92,39 @@ public class AwsAttachedDiskResourceBuilder extends AbstractAwsComputeBuilder {
         return ResourceType.AWS_VOLUMESET;
     }
 
+    @Override
+    protected List<CloudResourceStatus> checkResources(ResourceType type, AwsContext context, AuthenticatedContext auth, Iterable<CloudResource> resources) {
+
+        AmazonEC2Client client = awsClient.createAccess(auth.getCloudCredential());
+        List<CloudResource> volumeResources = StreamSupport.stream(resources.spliterator(), false)
+                .filter(r -> r.getType().equals(resourceType()))
+                .collect(Collectors.toList());
+        List<String> volumeIds = volumeResources.stream()
+                .map(this::getVolumeId)
+                .collect(Collectors.toList());
+
+        DescribeVolumesRequest describeVolumesRequest = new DescribeVolumesRequest(volumeIds);
+        DescribeVolumesResult result = client.describeVolumes(describeVolumesRequest);
+        return result.getVolumes().stream()
+                .map(v -> new CloudResourceStatus(findMatchingResource(volumeResources, v.getVolumeId()), toResourceStatus(v.getState())))
+                .collect(Collectors.toList());
+    }
+
+    private ResourceStatus toResourceStatus(String awsResourceStatus) {
+        if("available".equals(awsResourceStatus.toLowerCase())){
+            return ResourceStatus.CREATED;
+        }
+        return ResourceStatus.IN_PROGRESS;
+    }
+
+    private String getVolumeId(CloudResource r) {
+        return r.getParameter(VOLUME_ID, String.class);
+    }
+
+    private CloudResource findMatchingResource(List<CloudResource> volumeResources, String id) {
+        return volumeResources.stream().filter(v -> getVolumeId(v).equals(id)).findFirst().orElse(null);
+    }
+    
     @Override
     public int order() {
         return 1;
